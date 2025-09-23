5ad7e113a5b5876fcb2d16bba626af8def2a57a5
nehamerlin <g.nehamerlin@waib3tech.com>
2025-09-23 11:01:09 +0530

    division

--- division/routes.py
+++ division/routes.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
 
 div = Blueprint("division",__name__,template_folder="templates")
 
 @div.route("/div")
 def index():
   return render_template("division.html")
+
+@div.route("/divnum",methods=['POST'])
+def divnum():
+  num1 = int(request.form['num1'])
+  num2 = int(request.form['num2'])
+  res = num1 / num2
+  return render_template("division.html",result = res)
